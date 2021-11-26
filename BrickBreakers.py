6cd45dc58053daac5ca38377b4ef8556921dab93
Jason Hong <dingdinghzq@msn.com>
2021-11-26 15:59:26 -0800

    brick breaker v0.1

--- BrickBreakers.py
+++ BrickBreakers.py
@@ -14,6 +14,13 @@ BAR_HEIGHT = 20
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+BAR_VELOCITY = 10
+BALL_VELOCITY = 5.0
+TIME_UNIT = 1
+bar_moving = 0
+
+game_start = False
+
 BG = pygame.transform.scale(pygame.image.load(os.path.join('Assets','background','city.jpg')),(WIDTH,HEIGHT))
 
 def adjustBrightness(color, value):
@@ -52,8 +59,45 @@ class Ball(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (30, 30))
         self.rect = self.image.get_rect()
         self.rect.center = pos
+        self.x_speed = BALL_VELOCITY
+        self.y_speed = -BALL_VELOCITY
 
+    def update(self):
+        if not game_start:
+            dx = bar_moving * BAR_VELOCITY
+            bar = barGroup.sprites()[0]
+            if dx != 0 and bar.rect.right + dx > WIDTH or bar.rect.left + dx < 0:
+                dx = 0
+            self.rect = self.rect.move(dx, 0)            
+        else:
+            dx = self.x_speed * TIME_UNIT
+            dy = self.y_speed * TIME_UNIT
+            if self.rect.top + dy < 0:          
+                self.y_speed = -self.y_speed 
+            if self.rect.left + dx < 0 or self.rect.right + dx > WIDTH:                 
+                self.x_speed = -self.x_speed
 
+            targetRect = self.rect.move(dx, dy)
+            for brick in brickGroup:
+                if targetRect.colliderect(brick.rect):
+                    brick.health -= 1
+                    if brick.health <= 0:
+                        brick.kill()
+                    self.x_speed = self.x_speed * (1 + (random.random()-0.5)*0.3)
+                    self.y_speed = -self.y_speed
+            
+            for bar in barGroup:
+                if targetRect.colliderect(bar):
+                    self.x_speed = self.x_speed * (1 + (random.random()-0.5)*0.1)
+                    self.y_speed = -self.y_speed
+
+            if self.rect.bottom + dy > HEIGHT:
+                self.kill()
+                lose()
+
+            dx = self.x_speed * TIME_UNIT
+            dy = self.y_speed * TIME_UNIT    
+            self.rect = self.rect.move(dx, dy)
 
 class Bar(pygame.sprite.Sprite):
     def __init__(self, pos):
@@ -74,6 +118,12 @@ class Bar(pygame.sprite.Sprite):
         pygame.draw.line(surface, shadow, (BAR_WIDTH, 0), (BAR_WIDTH, BAR_HEIGHT), 5)
         return surface
     
+    def update(self):
+        dx = bar_moving * BAR_VELOCITY
+        if dx != 0 and self.rect.right + dx > WIDTH or self.rect.left + dx < 0:
+            dx = 0
+        self.rect = self.rect.move(dx, 0)
+
 brickGroup = pygame.sprite.Group()    
 ballGroup = pygame.sprite.Group()    
 barGroup = pygame.sprite.Group()
@@ -85,7 +135,32 @@ def redrawWindow(surface:pygame.surface.Surface):
     barGroup.draw(surface)
     pygame.display.update()
 
+def lose():
+    # lost one life
+    global game_start
+    game_start = False
+    for bar in barGroup:
+        bar.kill()
+    ballGroup.add(Ball((600, 650)))
+    barGroup.add(Bar((600, 675)))
+
+def update(key_pressed):
+
+    global bar_moving
+    if key_pressed[pygame.K_LEFT]:
+        bar_moving = -1
+    elif key_pressed[pygame.K_RIGHT]:
+        bar_moving = 1
+    else:
+        bar_moving = 0
+
+    brickGroup.update()
+    ballGroup.update()
+    barGroup.update()
+
+
 def main():
+    global game_start
     pygame.display.set_caption("Brick Breaker")
     window = pygame.display.set_mode((WIDTH,HEIGHT))
     clock = pygame.time.Clock()
@@ -105,7 +180,10 @@ def main():
                 run = False
         
         keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_SPACE]:
+            game_start = True
 
+        update(keys_pressed)
         redrawWindow(window)       
 
 if __name__ == "__main__":
