a845c0566123c9569c66f3811a55228c49588d24
Yvonne Hong <yvonnejhong@hotmail.com>
2022-01-30 13:21:08 -0800

    finish draw grid

--- Tetris.py
+++ Tetris.py
@@ -5,38 +5,49 @@ import os
 WIDTH = 960
 HEIGHT = 720
 SPACE = 30
-VERTICAL_ROW = 10
-HORIZONTAL_ROW = 20
+NUM_COLUMN = 10
+NUM_ROW = 20
+GAME_WIDTH = SPACE * NUM_COLUMN
+GAME_HEIGHT = SPACE * NUM_ROW
+GAME_TOP = (HEIGHT - GAME_HEIGHT)/2
+GAME_LEFT = (WIDTH - GAME_WIDTH)/2
 
+class Tetris:
+    def __init__(self, surface):
+        self.surface = surface
+        self.field = []
+        for _ in range(NUM_ROW):
+            row = []
+            for _ in range(NUM_COLUMN):
+                row.append(-1)
+            self.field.append(row)
+            
+        self.field[4][3] = 1
+        self.field[4][4] = 1
+        
 
+    def drawGrid(self):
+        for i in range(NUM_ROW):
+            pygame.draw.line(self.surface,(0, 0, 0), (GAME_LEFT, SPACE * i + GAME_TOP), (GAME_LEFT + GAME_WIDTH, SPACE * i + GAME_TOP))
 
-def draw(self, surface):
-    rect = pygame.Rect(self.pos.x * SPACE + 1, self.pos.y * SPACE + 1, SPACE - 1, SPACE -1)
-    pygame.draw.rect(surface, self.color, rect)
-    if self.eyes:
-        eye_rect = pygame.Rect(self.pos.x * SPACE + 1 + 5, self.pos.y * SPACE + 1 + 8, 4, 4)
-        pygame.draw.rect(surface, (0,0,0), eye_rect)
-        eye_rect = pygame.Rect(self.pos.x * SPACE + 1 + 13, self.pos.y * SPACE + 1 + 8, 4, 4)
-        pygame.draw.rect(surface, (0,0,0), eye_rect)
+        for i in range(NUM_COLUMN):
+            pygame.draw.line(self.surface,(0, 0, 0), (GAME_LEFT + SPACE * i, GAME_TOP), (GAME_LEFT + SPACE * i, GAME_HEIGHT + GAME_TOP))
 
-def drawGrid(surface):
-    for i in range(HORIZONTAL_ROW):
-        pygame.draw.line(surface,(0, 0, 0), (0, SPACE * i), (HEIGHT, SPACE * i))
-        #pygame.draw.line(surface,(0, 0, 0), (SPACE * i, 0), (SPACE * i, HEIGHT))
-
-    for i in range(VERTICAL_ROW):
-        #pygame.draw.line(surface,(0, 0, 0), (0, SPACE * i), (WIDTH, SPACE * i))
-        pygame.draw.line(surface,(0, 0, 0), (SPACE * i, 0), (SPACE * i, WIDTH))
-
-def redrawWindow(surface):
-    surface.fill((255, 255, 255))
-    drawGrid(surface)
-    pygame.display.update()            
+        pygame.draw.rect(self.surface, (0, 0, 0), pygame.Rect(GAME_LEFT, GAME_TOP, GAME_WIDTH, GAME_HEIGHT), 3)
+        
+    def draw(self):
+        self.surface.fill((255, 255, 255))
+        self.drawGrid()
+        for i in range(NUM_ROW):
+            for j in range(NUM_COLUMN):
+                if self.field[i][j] > -1:
+                    pygame.draw.rect(self.surface, (255,0,0), pygame.Rect(GAME_LEFT + j * SPACE, GAME_TOP + i * SPACE, SPACE, SPACE))
+        pygame.display.update()            
 
 def main():
     pygame.display.set_caption("Tetris")
     window = pygame.display.set_mode((WIDTH, HEIGHT))
-   
+    tetris = Tetris(window)
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -45,6 +56,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        redrawWindow(window)      
+        tetris.draw()    
         
 main()
